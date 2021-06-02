b029834659e23a6e41e971ea0193cff85cdf8e1a
Marcio Gadelha <zagadelha@gmail.com>
2021-06-02 10:51:22 -0300

    Inital commit

---
+++ src/main/java/com/contactbook/ContactBookCoreApplication.java
@@ -0,0 +1,13 @@
+package com.contactbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactBookCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactBookCoreApplication.class, args);
+	}
+
+}
